2e7668e77345f82823069d98f9d2e679d41ded92
goranknezevic <knezevichgoran@gmail.com>
2017-07-01 13:27:03 +0200

    Added automatic saving to the config file after verification code retrival

--- tests/testConfigurationReadWrite.py
+++ tests/testConfigurationReadWrite.py
@@ -2,8 +2,10 @@ import unittest
 from integration.configuration import Configuration
 
 class TestConfigurationReadWrite(unittest.TestCase):
-    def __init__(self):
-        pass
 
     def test_set_property(self):
         Configuration().save_property('test_key', 'test_value', 'TEST_SECTION')
+
+
+if __name__ == '__main__':
+    unittest.main()
