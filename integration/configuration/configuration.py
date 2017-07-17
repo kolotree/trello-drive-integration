001e3d959849b6b561e248242793354b2d521fdc
goranknezevic <knezevichgoran@gmail.com>
2017-07-17 15:31:31 +0200

    Config file is included on different way

--- integration/configuration/configuration.py
+++ integration/configuration/configuration.py
@@ -1,15 +1,28 @@
 import configparser
 import os
-
-config_file = os.path.join(os.path.dirname(__file__), 'config.ini')
+import sys
 
 class Configuration():
     def __init__(self):
         pass
 
+    def get_config_file(self):
+        return self.find_data_file('config.ini')
+
+    def find_data_file(self, filename):
+        if getattr(sys, 'frozen', False):
+            # The application is frozen
+            datadir = os.path.dirname(sys.executable)
+        else:
+            # The application is not frozen
+            # Change this bit to match where you store your data files:
+            datadir = os.path.dirname(__file__)
+
+        return os.path.join(datadir, filename)
+
     def get_configuration(self):
         config = configparser.ConfigParser()
-        config.read(config_file)
+        config.read(self.get_config_file())
         return config
 
     def read_auth_data_from_config(self):
@@ -59,5 +72,5 @@ class Configuration():
     def save_property(self, key, value, section='OAUTH'):
         config = self.get_configuration()
         config.set(section, key, value)
-        with open(config_file, 'w') as configfile:
+        with open(self.get_config_file(), 'w') as configfile:
             config.write(configfile)
