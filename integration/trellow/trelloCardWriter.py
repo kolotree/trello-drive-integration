0d95d86a8ad3f1647a3c99173bfeb2680b96bb8a
Stanko Culaja <culaja@gmail.com>
2017-07-17 12:06:23 +0200

    changed format of card names.

--- integration/trellow/trelloCardWriter.py
+++ integration/trellow/trelloCardWriter.py
@@ -6,9 +6,9 @@ class TrelloCardWriter:
         self.trelloClientWrapper = ClientFactory().getClient()
 
     def add_invoice_for(self, company, invoice):
-        card_name = company.name + ': ' + invoice.name
-        card_description = 'My description'
-        if self.trelloClientWrapper.add_card_conditionaly(card_name, card_description) == None:
+        card_name = company.name + ': ' + invoice.name + '(' + invoice.id + ')'
+        card_description = ''
+        if self.trelloClientWrapper.add_card_conditionaly(card_name, card_description) is None:
             return False
         else:
             return True
