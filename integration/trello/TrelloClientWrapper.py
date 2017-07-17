f033d2e2d083783fa6eb873d597f6be801e2fd32
Stanko Culaja <culaja@gmail.com>
2017-07-17 10:45:27 +0200

    Integrated trello client wrapper with rest of the code. Draft implementation.

--- integration/trello/TrelloClientWrapper.py
+++ integration/trello/TrelloClientWrapper.py
@@ -1,4 +1,4 @@
-from trello import TrelloClient
+from trello.trelloclient import TrelloClient
 
 class TrelloClientWrapper(TrelloClient):
 
--- integration/trello/trelloCardWriter.py
+++ integration/trello/trelloCardWriter.py
@@ -1,3 +1,14 @@
+from .clientFactory import ClientFactory
+
+
 class TrelloCardWriter:
+    def __init__(self):
+        self.trelloClientWrapper = ClientFactory().getClient()
+
     def add_invoice_for(self, company, invoice):
-        return True
+        card_name = company.name + ': ' + invoice.name
+        card_description = 'My description'
+        if self.trelloClientWrapper.add_card_conditionaly(card_name, card_description) == None:
+            return False
+        else:
+            return True
