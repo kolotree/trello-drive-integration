b2eb4e40588838473adc73eeff8d48b825783f10
goranknezevic <knezevichgoran@gmail.com>
2017-07-20 16:25:33 +0200

    Integration with GDrive part

--- integration/trellow/trelloCardWriter.py
+++ integration/trellow/trelloCardWriter.py
@@ -7,4 +7,7 @@ class TrelloCardWriter:
 
     def add_invoices_for(self, company, input_invoices_count, output_invoices_count):
         print ('Company: %s, UF: %s, IF: %s' % (company.name, str(input_invoices_count), str(output_invoices_count)))
-        return True
+        card_name = '{0} (UF:{1} | IF:{2})'.format(company.name, input_invoices_count, output_invoices_count)
+        card_description = '{0} - ID: {1}'.format(company.name, company.id)
+        card = self.trelloClientWrapper.add_or_update_card(card_name, card_description)
+        return False if card == [] or card == None else True
