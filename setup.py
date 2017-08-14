846a5ad7899a0f65c8d37e675b90353625fd0c78
GKnezevic <goran.knezevic@kolotree.com>
2017-08-14 15:57:58 -0500

    setup changed to extract config.ini

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from cx_Freeze import setup, Executable
 
+includefiles = ['integration/configuration/config.ini']
+
 executables = [
     Executable('integration/applicationEntryPoint.py')
 ]
 
 setup(name='trello-gdrive',
-      options = {"build_exe": {"packages":["idna"]}},
-      version='0.1',
+      options = {"build_exe": {"packages":["idna"],'include_files':includefiles}},
+      version='0.2',
       description='Automated integration of G Drive with trello',
       executables=executables
       )
